f45758fc2d6d6a732f0d9483995acd8953e9302a
Amal Bose <axatrikx@gmail.com>
2014-04-29 19:35:00 +0530

    Added Spring jump

---
+++ Assets/Scripts/LevelItems/SpringScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpringScript : MonoBehaviour
+{
+
+	public float force = 2000f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.gameObject.name == "Player")
+			col.gameObject.rigidbody2D.AddForce (new Vector2 (0, force));
+	}
+}
